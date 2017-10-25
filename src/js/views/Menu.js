37b375ef31435ac42e1999a2e5f0f4b2c1d412a7
Steve Russo <steverusso@protonmail.com>
2017-10-25 18:24:13 -0400

    Removed unused import.

--- src/js/views/Menu.js
+++ src/js/views/Menu.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col } from 'react-materialize';
+import { Row } from 'react-materialize';
 import MenuItem from '../components/MenuItem'
 import MenuData from '../data/MenuData'
 
