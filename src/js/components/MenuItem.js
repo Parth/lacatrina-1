6702cfb010de2b43fd34033c96d349f02773d44b
Steve Russo <steverusso@protonmail.com>
2017-10-24 20:36:01 -0400

    Made a 'MenuItem' component with some initial code.

---
+++ src/js/components/MenuItem.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Row, Col, Card, MediaBox } from 'react-materialize';
+
+class MenuItem extends Component {
+	render() {
+		return (
+			<Col s={12} m={6}>
+				<Card className="menuItem">
+					<Row>
+						<Col s={4}>
+							<MediaBox src={this.props.img} caption={this.props.desc} height="128"/>
+						</Col>
+						<Col s={8} style={{height: '100%'}} className="valign-wrapper">
+							<div>
+								<h4>{this.props.title}</h4>
+							</div>
+						</Col>
+					</Row>
+					<Row>
+						<Col s={12}>
+							<p>{this.props.desc}</p>
+						</Col>
+					</Row>
+					<Row>
+						<Col s={12} m={6}>
+							<ul>
+							{this.props.prices.map((price) =>
+								<li key={price.toString()}>
+									{price}
+								</li>
+							)}
+							</ul>
+						</Col>
+						<Col s={12} m={6} className="right-align">
+							<br/>
+							<a href="#">Order Now on Grubhub!</a>
+						</Col>
+					</Row>
+				</Card>
+			</Col>
+		);
+	}
+}
+
+export default MenuItem;
