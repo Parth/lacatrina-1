2a692cf63ff050fca8d6a57f411c49f515383918
Steve Russo <steverusso@protonmail.com>
2017-10-24 20:37:21 -0400

    Started messing with the 'Menu' view.

--- src/js/views/Menu.js
+++ src/js/views/Menu.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'react-materialize';
+import MenuItem from '../components/MenuItem'
 
 class Menu extends Component {
 	render() {
-		return <div>Menu</div>
+		return (
+			<div className="container">
+				<h2>Menu</h2>
+				<Row>
+					<MenuItem img="img/tacos.jpg" title="Tacos" desc="Fresh made corn tortillas filled with choice of meat, cilantro and onions" prices={[ "1 @ $2.50", "3 @ $7" ]}/>
+				</Row>
+			</div>
+		);
 	}
 }
 
