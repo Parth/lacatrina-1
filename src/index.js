9ef8bd2568887b7ac49452fcdbd63d34a3b50909
Steve Russo <steverusso@protonmail.com>
2017-10-29 18:59:29 -0400

    Added imports.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom'
 import App from './js/App';
 
+import 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
 import './css/index.css';
 
 ReactDOM.render((
